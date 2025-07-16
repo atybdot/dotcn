b3893ad70bf58e9d81dad5539f7459b971f1d470
Atyb. <180611249+atybdot@users.noreply.github.com>
2025-07-16 12:09:57 +0530

    chore:basic repo setup

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
+  entry: ["./src/index.ts",],
   platform: "node",
   dts: {
     isolatedDeclarations: true,
   },
-})
+});
