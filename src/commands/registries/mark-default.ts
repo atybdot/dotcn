13b50a2229dfd317aaa320bb6aad2d3715cf0a25
Atyb. <180611249+atybdot@users.noreply.github.com>
2025-07-16 20:05:11 +0530

    feat:better mark-default command

--- src/commands/registries/mark-default.ts
+++ src/commands/registries/mark-default.ts
@@ -1,8 +1,9 @@
-import fs from "node:fs";
+import path from "node:path";
 import { Command } from "commander";
 import { FILE_NAME } from "@/constants";
 import type { RegistriesSchema } from "@/schema";
-import { readRegistry } from "@/utils";
+import { readRegistry, writeToFile } from "@/utils";
+import { highlighter, logger } from "@/utils/highlighter";
 export const markDefaultCmd = new Command("mark-default");
 markDefaultCmd
   .description("mark registry as default ")
@@ -10,22 +11,41 @@ markDefaultCmd
   .option("-c,--cwd <cwd>", "specify folder for registries.json", process.cwd())
   .action((name, opts) => {
     const values = readRegistry(opts.cwd);
+    const basePath = path.resolve(opts.cwd);
+    const registryPath = `${basePath}/${FILE_NAME}`;
     const valuePresent = values.registries?.find((reg) => reg.name === name);
     if (!valuePresent) {
-      console.error(
-        `${name} is not registered.\nIf you are trying to add a registery then run \nnpx dotcn add ${name} <url>`
+      logger.error(
+        `❌ ${highlighter.warn(
+          highlighter.underline(name)
+        )} is not present in ${highlighter.underline(
+          FILE_NAME
+        )}.\nIf you are trying to add a registery then run \n${highlighter.warn(
+          `npx dotcn add ${name} <url>`
+        )}`
       );
       process.exit(0);
     }
     if (values.default.name === name) {
-      console.log(
-        `${name} is already marked as default\nif you are trying to remove it\nrun npx dotcn remove ${name}`
+      logger.success(
+        `${name} is already marked as ${highlighter.underline("Default")} ✅`
       );
+      logger.info("If you are trying to remove it run");
+      logger.warn(`npx dotcn remove ${name}`);
       process.exit(0);
     }
     const newData: RegistriesSchema = {
       default: values.registries.find((reg) => reg.name === name)!,
       registries: values?.registries,
     };
-    fs.writeFileSync(FILE_NAME, JSON.stringify(newData, null, 2));
+    const writeSuccess = writeToFile(registryPath, newData, true, true);
+    if (!writeSuccess) {
+      logger.error(
+        `Unable to mark ${highlighter.underline(name)} as Default ❌`
+      );
+      return;
+    }
+    logger.success(
+      `Successfully marked ${highlighter.underline(name)} as Default ✅`
+    );
   });
